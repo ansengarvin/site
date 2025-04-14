a36b477f4a17e0bf8cc47e729ba0b72a8a84c8e9
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-04-13 21:28:16 -0700

    refactor: removed unused d&d route and file

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,6 @@ import {Projects} from "./routes/portfolio.js"
 import {Other} from "./routes/other.js"
 import {Connect} from "./routes/connect.js"
 import { Recipes } from "./routes/recipes.js"
-import { DnD } from "./routes/dnd.js"
 
 export const router = createBrowserRouter([
     {
@@ -27,11 +26,9 @@ export const router = createBrowserRouter([
         {path: "connect", element: <Connect />},
         // Other Routes
         {path: "recipes", element: <Recipes />},
-        {path: "d&d", element: <DnD/>},
         // Redirects
         {path: "contact", element: <Navigate to="/connect"/>},
         {path: "projects", element: <Navigate to="/portfolio"/>},
-        {path: "dnd", element: <Navigate to="/d&d"/>},
       ]
   }
 ])
