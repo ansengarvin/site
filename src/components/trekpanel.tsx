667440ccedbfd88622b8e2e4069c01d000ee9aec
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-04-13 21:16:16 -0700

    chore: removed unused imports from trekpanel

--- src/components/trekpanel.tsx
+++ src/components/trekpanel.tsx
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
 
-import { color_background, color_element, color_element_text, color_offwhite, color_title, color_gradient_A, color_title_hover, color_title_active} from "../lib/defines/colors";
+import { color_background, color_element, color_offwhite, color_title, color_gradient_A} from "../lib/defines/colors";
 import { laptop, tablet, phone } from "../lib/defines/screenWidths";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { NavBar } from "./navbar";
 import { navWidthDesktop, navWidthPhone, navWidthTablet } from "../lib/defines/navWidths";
